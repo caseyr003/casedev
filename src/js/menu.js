24442e3a5eccda57c6d41f5467dd6d920c6e16dc
Casey Ross <caseyross003@gmail.com>
2017-05-15 09:32:05 -0500

    feat: Animate menu icon

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,9 @@
 $( function() {
-
-  $( '.menu-toggle' ).on( 'click', function() {
-    $( '.menu-item' ).toggleClass( 'menu-open' );
-    $( '.overlay' ).toggleClass( 'overlay-open' );
-  });
-
+   $( '.menu-toggle' ).on( 'click', function() {
+      $( '.menu-item' ).toggleClass( 'menu-open' );
+      $( '.overlay' ).toggleClass( 'overlay-open' );
+      $( '.top' ).toggleClass( 'top-open' );
+      $( '.middle' ).toggleClass( 'middle-open' );
+      $( '.bottom' ).toggleClass( 'bottom-open' );
+   });
 });
