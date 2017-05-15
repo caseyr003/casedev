e77627c099cf6a892585bd58fb315c12065e1c5b
Casey Ross <caseyross003@gmail.com>
2017-05-15 13:45:54 -0500

    refactor: Update home header

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,4 +6,11 @@ $( function() {
       $( '.middle' ).toggleClass( 'middle-open' );
       $( '.bottom' ).toggleClass( 'bottom-open' );
    });
+   $( '.overlay' ).on( 'click', function() {
+      $( '.menu-item' ).removeClass( 'menu-open' );
+      $( '.overlay' ).removeClass( 'overlay-open' );
+      $( '.top' ).removeClass( 'top-open' );
+      $( '.middle' ).removeClass( 'middle-open' );
+      $( '.bottom' ).removeClass( 'bottom-open' );
+   });
 });
